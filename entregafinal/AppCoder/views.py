84fa0b108f980f12879145898dde6ab4d9121246
candelascarzello951 <candelascarzelllo1@gmail.com>
2023-12-03 12:24:17 -0300

    actualizaciones

--- entregafinal/AppCoder/views.py
+++ entregafinal/AppCoder/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def inicio_view(xx):
     return HttpResponse("Bienvenidos!!!!!!!!!!!!!!")
 
 
 def cursos_view(xx):
-    # return HttpResponse("Aquí voy a mostrar mis cursos")
-    # return HttpResponse("Bienvenido")
+    #return HttpResponse("Aquí voy a mostrar mis cursos")
     return render(xx, "AppCoder/padre.html")
